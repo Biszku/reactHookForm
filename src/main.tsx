afaabd4a9dcd86514fdb032cb40baf13fe13b739
Biszku <dbichu@onet.pl>
2023-11-29 21:47:08 +0100

    created basic form

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
+import { useForm } from "react-hook-form";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      example: "",
+      exampleRequired: "",
+    },
+  });
+
+  console.log(watch("example"));
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        alert(JSON.stringify(data));
+      })}
+    >
+      <label>Example</label>
+      <input {...register("example", { required: true, maxLength: 10 })} />
+      {errors.example && <p>This field is required</p>}
+      <label>ExampleRequired</label>
+      <input
+        {...register("exampleRequired", { required: true, maxLength: 10 })}
+      />
+      {errors.exampleRequired && <p>This field is required</p>}
+      <input type="submit" />
+    </form>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
