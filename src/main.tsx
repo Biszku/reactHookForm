a44beab1c34fdda83ee28a40e27b38d3788640ed
Biszku <dbichu@onet.pl>
2023-11-30 12:08:02 +0100

    added zod validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,48 @@
 import "./index.css";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import ReactDOM from "react-dom";
+import { z } from "zod";
+
+const signUpSchema = z
+  .object({
+    name: z.string().min(1, "Invalid name"),
+    email: z.string().email(),
+    password: z.string().min(10, "Password must be at least 10 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords must mach",
+    path: ["confirmPassword"],
+  });
+
+type TSignUpScheme = z.infer<typeof signUpSchema>;
 
 function App() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm({
-    defaultValues: {
-      example: "",
-      exampleRequired: "",
-    },
+  } = useForm<TSignUpScheme>({
+    resolver: zodResolver(signUpSchema),
   });
 
-  console.log(watch("example"));
+  const onSubmit = (data: TSignUpScheme) => alert(JSON.stringify(data));
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        alert(JSON.stringify(data));
-      })}
-    >
-      <label>Example</label>
-      <input {...register("example", { required: true, maxLength: 10 })} />
-      {errors.example && <p>This field is required</p>}
-      <label>ExampleRequired</label>
-      <input
-        {...register("exampleRequired", { required: true, maxLength: 10 })}
-      />
-      {errors.exampleRequired && <p>This field is required</p>}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input {...register("name")} />
+      {errors.name && <p>{`${errors.name.message}`}</p>}
+      <label>Email</label>
+      <input {...register("email")} />
+      {errors.email && <p>{`${errors.email.message}`}</p>}
+      <label>Password</label>
+      <input {...register("password")} type="password" />
+      {errors.password && <p>{`${errors.password.message}`}</p>}
+      <label>Confirm Password</label>
+      <input {...register("confirmPassword")} type="password" />
+      {errors.confirmPassword && <p>{`${errors.confirmPassword.message}`}</p>}
       <input type="submit" />
     </form>
   );
